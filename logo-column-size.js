cb35347518ef14e73505dbd4410772107ea5e3b6
Goldberg <mgoldberg@npv-23115-imac.carsdirect.win>
2018-07-23 12:21:45 -0400

    initial

---
+++ logo-column-size.js
@@ -0,0 +1,16 @@
+<script>
+
+jQuery(function($){
+
+var $logocolumn = $('.con-z01-middle .header-logo');
+var $phonecolumn = $('con-z01-middle .header-phone');
+
+// Adjust the medium-8 & medium-4 to any column structure, but it must = 12 total.
+
+$logocolumn.removeClass('medium-6').addClass('medium-8');
+$phonecolumn.removeClass('medium-6').addClass('medium-4');
+
+
+});
+
+</script>
